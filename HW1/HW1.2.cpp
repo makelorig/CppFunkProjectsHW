4e27ce4487df9b52b9a358c9e9a8727e453e1b38
makelorig <106504877+makelorig@users.noreply.github.com>
2023-09-19 12:10:25 +0300

    Add files via upload

---
+++ HW1/HW1.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    /*float i, p, g, d;
+    cin >> p;
+    cin >> g;
+    cin >> i;
+    d = (p + g + i) / 3;
+    cout << "Ваше среднее арифмет:"<<round(d);*/
+    int i=0, b=0;
+    float r, a;
+    while (i < 4)
+    {
+        cout << "Введите оценку за четверть  1:\n";
+        cin >> a;
+        b = b + a;
+        i = i + 1;
+        cout << "Введите оценку за четверть  2:\n";
+        cin >> a;
+        b = b + a;
+        i= i + 1;
+        cout << "Введите оценку за четверть  3:\n";
+        cin >> a;
+        b = b + a;
+        i = i + 1;
+        cout << "Введите оценку за четверть  4:\n";
+        cin >> a;
+        b = b + a;
+        i = i + 1;
+    }
+    r = b / 4;
+    cout << "Оценка годовая:\n" << round(r);
+}
