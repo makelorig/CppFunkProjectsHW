49c6efac8b62e6116b432228377a19e34e8d1ef2
makelorig <106504877+makelorig@users.noreply.github.com>
2023-09-19 12:29:37 +0300

    Add files via upload

---
+++ HW1/HW1.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int i = 0, b = 0;
+    float a;
+    while (i < 3)
+    {
+        cout << "Зарплата за месяц 1:\n";
+        cin >> a;
+        b = b + a;
+        i = i + 1;
+        cout << "Зарплата за месяц 2:\n";
+        cin >> a;
+        b = b + a;
+        i = i + 1;
+        cout << "Зарплата за месяц 3:\n";
+        cin >> a;
+        b = b + a;
+        i = i + 1;
+    }
+    cout << "Зарплата за квартал:\n" << b;
+}
