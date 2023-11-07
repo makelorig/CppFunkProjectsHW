c71aeb1c446514846ae7b396763d128831de95dc
makelorig <106504877+makelorig@users.noreply.github.com>
2023-11-07 11:47:19 +0300

    Update HW6.1.cpp

--- HW6/HW6.1.cpp
+++ HW6/HW6.1.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
 
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int x, y, uo = 0;
+	bool flag;
+
+	cout << "Введите диапазоны: ";
+	cin >> x >> y;
+
+	if (x > y)
+		swap(x, y);
+
+	for (int i = x; i <= y; i++) {
+		flag = true;
+		for (int j = 2; j <= sqrt(i); j++) {
+			if (i % j == 0) {
+				flag = false;
+			}
+		}
+		if (flag)
+			uo++;
+	}
+
+	cout << x << " to " << y << " is " << uo;
+}
