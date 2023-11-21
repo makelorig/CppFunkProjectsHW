75d6e5d3b1cc34e2c26be2c204d7c11e3fac4110
makelorig <106504877+makelorig@users.noreply.github.com>
2023-11-21 23:35:53 +0300

    Update HW7.1.cpp

--- HW7/HW7.1.cpp
+++ HW7/HW7.1.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
 
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+    int arr[10] = {};
+    int min = 11, max = 0;
+
+    for (int i = 0; i < 10; i++) {
+        arr[i] = 1 + rand() % 10;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (min >= arr[i])
+            min = arr[i];
+        if (max <= arr[i])
+            max = arr[i];
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout  << "Минимальное число: " << min << ", максимальное число: " << max;
+}
